28ef323716f4907968d22bf605aec2caa4ba06d1
Meg MacIver <megmaciver13@gmail.com>
2017-10-24 16:45:47 -0400

    create initial html file

---
+++ game-files/gf-tool.js
@@ -0,0 +1,3 @@
+var startButton = document.getElementsByClassName('start-button')
+var answerSubmission = document.getElementsById('answer').value
+var submissionButton = document.getElementsByClassName('submission-button')
