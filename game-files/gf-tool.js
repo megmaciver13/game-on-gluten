e92b60b80bdab98350a84c6d408280718fb9445e
Meg MacIver <megmaciver13@gmail.com>
2017-10-25 16:02:10 -0400

    show multiple choice answer fields

--- game-files/gf-tool.js
+++ game-files/gf-tool.js
@@ -3,117 +3,88 @@ var questionContainer = document.querySelector('.question-container')
 var answerContainer = document.querySelector('.answer-container')
 var submissionButton = document.querySelector('.submission-button')
 var score = 0
-var currentQuestion = []
-var currentAnswerChoices = []
+var currentQuestion
+var currentAnswerChoices
 
 startButton.addEventListener('click', generateNextQuestion)
 
 function generateNextQuestion () {
-  currentQuestion.push(triviaQuestions[0].question)
-  currentAnswerChoices.push(triviaQuestions[0].answerChoices)
+  currentQuestion = triviaQuestions[0].question
+  currentAnswerChoices = triviaQuestions[0].answerChoices // instead of replacing array entirely, should I push these consecutively in a for loop?
   return showQuestion()
 }
 
 function showQuestion () {
-  questionContainer.innerHTML = currentQuestion[0]
+  questionContainer.innerHTML = currentQuestion
   return showAnswerChoices()
 }
 
 function showAnswerChoices () {
-  answerContainer.innerHTML = currentAnswerChoices[0]
+  answerContainer.style.display = 'initial'
+  for (var i = 0; i < currentAnswerChoices.length; i++ ) {
+    answerContainer.getElementsByClassName('choice[i]').innerHTML = currentAnswerChoices[i]
+  }
 }
+  // for (var i = 0; i <= currentAnswerChoices.length; i++) {
+  //   var answerField = document.createElement("input") // how to create various attributes of input field? (and what attributes are necessary?)
+  //   answerField.setAttribute("type", "radio")
+  //   var lineBreak = document.createElement("br")
+  //   currentAnswerChoices[i]
+  //   answerContainer.append(answerField)
+  //   answerContainer.append(lineBreak)
+  // }
 
-// function submit () {}
+// submission-button.addEventListener('click', function submit () {})
 
 var triviaQuestions = [
   {
     question: 'Question 1',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'b'
   },
   {
     question: 'Question 2',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'b'
   },
   {
     question: 'Question 3',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'c'
   },
   {
     question: 'Question 4',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'a'
   },
   {
     question: 'Question 5',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'a'
   },
   {
     question: 'Question 6',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'b'
   },
   {
     question: 'Question 7',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'a'
   },
   {
     question: 'Question 8',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'c'
   },
   {
     question: 'Question 9',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'a'
   },
   {
     question: 'Question 10',
-    answerChoices: {
-      a: 'A',
-      b: 'B',
-      c: 'C'
-    },
+    answerChoices: ['A', 'B', 'C'],
     correctAnswer: 'b'
   }
 ]
