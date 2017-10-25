06de67f6dd148faa9c01560e5ce369ef417513d5
Meg MacIver <megmaciver13@gmail.com>
2017-10-25 09:53:51 -0400

    create questions object

--- game-files/gf-tool.js
+++ game-files/gf-tool.js
@@ -1,3 +1,105 @@
 var startButton = document.getElementsByClassName('start-button')
-var answerSubmission = document.getElementsById('answer').value
+var questionContainer = document.getElementsByClass('question-container').value
+var answerContainer = document.getElementsByClass('answer-container').value
 var submissionButton = document.getElementsByClassName('submission-button')
+var score = 0
+
+var triviaQuestions = [
+  {
+    question: 'Question 1',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: 'Question 2',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: 'Question 3',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Question 4',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Question 5',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Question 6',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: 'Question 7',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Question 8',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Question 9',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Question 10',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'b'
+  }
+]
+
+startButton.addEventListener('click', nextQuestion())
+
+function nextQuestion () {
+  for (var i = 0; i<=10; i++)
+    triviaQuestions[i].question
+}
