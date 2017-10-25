a390abd770406430b940b2693fe1501e311cebd7
Meg MacIver <megmaciver13@gmail.com>
2017-10-25 11:12:48 -0400

    add start button event listener

--- game-files/gf-tool.js
+++ game-files/gf-tool.js
@@ -1,8 +1,18 @@
-var startButton = document.getElementsByClassName('start-button')
-var questionContainer = document.getElementsByClass('question-container').value
-var answerContainer = document.getElementsByClass('answer-container').value
-var submissionButton = document.getElementsByClassName('submission-button')
+var startButton = document.querySelector('.start-button')
+var questionContainer = document.querySelector('.question-container').value
+var answerContainer = document.querySelector('.answer-container').value
+var submissionButton = document.querySelector('.submission-button')
 var score = 0
+
+startButton.addEventListener('click', nextQuestion)
+
+function nextQuestion () {
+  // for (var i = 0; i<=10; i++)
+  //   questionContainer = triviaQuestions[i].question
+  //   answerContainer = triviaQuestions[i].answerChoices
+}
+
+// function submit () {}
 
 var triviaQuestions = [
   {
@@ -96,13 +106,3 @@ var triviaQuestions = [
     correctAnswer: 'b'
   }
 ]
-
-startButton.addEventListener('click', nextQuestion())
-
-function nextQuestion () {
-  for (var i = 0; i<=10; i++)
-    questionContainer = triviaQuestions[i].question
-    answerContainer = triviaQuestions[i].answerChoices
-}
-
-// function submit () {}
