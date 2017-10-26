6c87120eb6fdf2853cc00fe5388ac8d54271a7c5
Meg MacIver <megmaciver13@gmail.com>
2017-10-26 14:31:21 -0400

    create new js file to store questions

---
+++ game-files/questions.js
@@ -0,0 +1,92 @@
+var triviaQuestions = [
+  {
+    question: 'Question 1',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: 'Question 2',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: 'Question 3',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Question 4',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Question 5',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Question 6',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: 'Question 7',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Question 8',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Question 9',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Question 10',
+    answerChoices: {
+      a: 'A',
+      b: 'B',
+      c: 'C'
+    },
+    correctAnswer: 'b'
+  }
+]
