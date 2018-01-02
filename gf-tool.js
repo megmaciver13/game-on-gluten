f8808e12ed00bf51df13e64b65136e0bae28decb
Meg MacIver <megmaciver13@gmail.com>
2018-01-02 13:33:21 -0500

    added separated questions between categories

--- gf-tool.js
+++ gf-tool.js
@@ -1,178 +1,182 @@
-// define variables
-var currentQuestionSet = []
-var startButton = document.querySelector('#start-button')
-var questionContainer = document.querySelector('.question-container')
-var answerContainer = document.querySelector('.answer-container')
-var nextButton = document.querySelector('.next')
-var scoreContainer = document.querySelector('.trivia-score')
-var returnHomeButton = document.querySelector('#return-home')
-var timerText = document.querySelector('.trivia-timer')
-var timer = 60
-var score = 0
-var currentQuestion
-var currentAnswerChoices = []
-var counter = 0
+function runGame () {
+  // define variables
+  var currentQuestionSet = []
+  var startButton = document.querySelector('#start-button')
+  var questionContainer = document.querySelector('.question-container')
+  var answerContainer = document.querySelector('.answer-container')
+  var nextButton = document.querySelector('.next')
+  var scoreContainer = document.querySelector('.trivia-score')
+  var returnHomeButton = document.querySelector('#return-home')
+  var timerText = document.querySelector('.trivia-timer')
+  var timer = 60
+  var score = 0
+  var currentQuestion
+  var currentAnswerChoices = []
+  var counter = 0
 
-// create event listener for start button to prompt the user to choose a category
-startButton.addEventListener('click', chooseCategory)
+  // create event listener for start button to prompt the user to choose a category
+  startButton.addEventListener('click', chooseCategory)
 
-// create function to choose category
-function chooseCategory () {
-  startButton.style.display = 'none'
-  nextButton.style.display = 'none'
-  answerContainer.style.display = 'inline'
-  var basicQs = '<div class="category" id="celiac">What is Celiac Disease?</div>'
-  var foodQs = '<div class="category" id="eat">Can I Eat This?</div>'
-  questionContainer.innerHTML = "Choose which category you'd like to play:"
-  answerContainer.innerHTML = basicQs + '<br>' + foodQs
-  answerContainer.addEventListener('click', setChosenCategoryQuestions)
-}
-
-// function to listen for which category in answer container was chosen and start timer
-function setChosenCategoryQuestions (e) {
-  if (e.target.id === 'celiac') {
-    setQuestionsToCeliacCategory()
-    startTimer()
-  } else if (e.target.id === 'eat') {
-    setQuestionsToFoodCategory()
-    startTimer()
-  }
-}
-
-function setQuestionsToCeliacCategory () {
-  currentQuestionSet = celiacQuestions
-  showQuestions()
-}
-
-function setQuestionsToFoodCategory () {
-  currentQuestionSet = foodQuestions
-  showQuestions()
-}
-
-// create function to take down the start message and generate the first trivia question
-function showQuestions () {
-  currentQuestion = `${currentQuestionSet[counter].question}`
-  questionContainer.innerHTML = currentQuestion
-  return showAnswers()
-}
-
-// create a function to generate the answer choices
-function showAnswers () {
-  answerContainer.style.display = 'inline'
-  currentAnswerChoices.push(`<div class='choicea'> A : ${currentQuestionSet[counter].answerChoices.a}</div>`)
-  currentAnswerChoices.push(`<div class='choiceb'> B : ${currentQuestionSet[counter].answerChoices.b}</div>`)
-  currentAnswerChoices.push(`<div class='choicec'> C : ${currentQuestionSet[counter].answerChoices.c}</div>`)
-  // attempt at a for loop to keep code DRY
-  // for (letter in currentQuestionSet[counter].answerChoices) {
-  //   currentAnswerChoices.push("<div class='choice'" + `${letter}` + ">" + `${letter}` + " : " + `${currentQuestionSet[counter].answerChoices.letter}` + "</div>")
-  // }
-  answerContainer.innerHTML = currentAnswerChoices.join('<br>')
-}
-
-// create event listener for each answer choice to change color when the mouse hovers
-answerContainer.addEventListener('mouseover', function (e) {
-  e.target.style.color = '#a2c1a4'
-})
-
-// create event listener for each answer choice to change color back when the hovering mouse leaves
-answerContainer.addEventListener('mouseout', function (e) {
-  e.target.style.color = '#55595d'
-})
-
-// create event listener to submit answer choice, show a message that the message is either right or wrong, and generate a next question button
-answerContainer.addEventListener('click', function (e) {
-  var wrongDiv = `<div class='wrong'>Not quite!</div>`
-  var correctDiv = `<div class='correct'>Correct! You have been awarded 1 point!</div>`
-  if (e.target.className === `choice${currentQuestionSet[counter].correctAnswer}`) {
-    answerContainer.style.display = 'none'
-    questionContainer.innerHTML = correctDiv
-    nextButton.style.display = 'inline'
-    score += 1
-    scoreContainer.innerHTML = score
-  } else {
-    answerContainer.style.display = 'none'
-    questionContainer.innerHTML = wrongDiv
-    nextButton.style.display = 'inline'
-  }
-})
-
-// create an event listener to generate the next question
-nextButton.addEventListener('click', generateNextQuestion)
-
-// attempt at event listener to click enter for 'next question' button:
-// if (questionContainer.innerHTML === wrongDiv || questionContainer.innerHTML === correctDiv) {
-//   window.addEventListener('keypress', generateNextQuestion)
-// }
-
-// increase the counter number to iterate through the next question and answers in the triviaQuestions array
-function generateNextQuestion () {
-  if (counter < 9) {
-    currentAnswerChoices = []
-    counter += 1
-    showQuestions()
-  } else {
-    counter += 1
-    showFinalScore()
-  }
-}
-
-// show final score and corresponding message
-function showFinalScore () {
-  stopTimer()
-  timer = 0
-  timerText.innerHTML = '00:00'
-  if (score === 0) {
+  // create function to choose category
+  function chooseCategory () {
+    startButton.style.display = 'none'
     nextButton.style.display = 'none'
-    questionContainer.innerHTML = 'Game over! Unfortunately, you did not score. You need to brush up on your gluten knowledge!'
-  } else if (score < (counter - 5)) {
-    nextButton.style.display = 'none'
-    questionContainer.innerHTML = `Your final score is ${score}. Looks like you need to brush up on your gluten knowledge.`
-  } else if (score <= counter) {
-    nextButton.style.display = 'none'
-    questionContainer.innerHTML = `Your final score is ${score}. Looks like you know a thing or two about gluten!`
+    answerContainer.style.display = 'inline'
+    var basicQs = '<div class="category" id="celiac">What is Celiac Disease?</div>'
+    var foodQs = '<div class="category" id="eat">Can I Eat This?</div>'
+    questionContainer.innerHTML = "Choose which category you'd like to play:"
+    answerContainer.innerHTML = basicQs + '<br>' + foodQs
+    answerContainer.addEventListener('click', setChosenCategoryQuestions)
   }
-  returnHomeButton.style.display = 'inline'
-}
 
-// create event listener for home button to reset to the start page
-returnHomeButton.addEventListener('click', resetPage)
-
-// create a function to reset to the start page to begin the quiz again
-function resetPage () {
-  questionContainer.innerHTML = "So you want to learn more about gluten? You've come to right place. Welcome to <a class='game-title'>Can I Eat This?</a>, a game on gluten. You'll learn all about this pervasive protein - what it is, what it's in, and how to avoid it. Click the button below to begin.<br>"
-  returnHomeButton.style.display = 'none'
-  startButton.style.display = 'inline'
-  counter = 0
-  score = 0
-  timer = 60
-  scoreContainer.innerHTML = score
-  currentQuestionSet = []
-  currentAnswerChoices = []
-}
-
-var gameTimer
-
-function stopTimer () {
-  console.log(gameTimer)
-  clearInterval(gameTimer)
-}
-
-// create timer
-function startTimer () {
-  gameTimer = setInterval(function () {
-    timer--
-    if (timer === 0) {
-      timerText.innerHTML = '00:00'
-      stopTimer()
-      if (counter <= 9) {
-        questionContainer.innerHTML = 'Game Over! It appears you are not a master of gluten knowledge. Try again!'
-        returnHomeButton.style.display = 'inline'
-        answerContainer.style.display = 'none'
-      }
-    } else if (timer < 10) {
-      timerText.innerHTML = '00:0' + timer
-    } else if (timer >= 10) {
-      timerText.innerHTML = '00:' + timer
+  // function to listen for which category in answer container was chosen and start timer
+  function setChosenCategoryQuestions (e) {
+    if (e.target.id === 'celiac') {
+      setQuestionsToCeliacCategory()
+      startTimer()
+    } else if (e.target.id === 'eat') {
+      setQuestionsToFoodCategory()
+      startTimer()
     }
-  }, 1000)
+  }
+
+  function setQuestionsToCeliacCategory () {
+    currentQuestionSet = celiacQuestions
+    showQuestions()
+  }
+
+  function setQuestionsToFoodCategory () {
+    currentQuestionSet = foodQuestions
+    showQuestions()
+  }
+
+  // create function to take down the start message and generate the first trivia question
+  function showQuestions () {
+    currentQuestion = currentQuestionSet[counter].question
+    questionContainer.innerHTML = currentQuestion
+    return showAnswers()
+  }
+
+  // create a function to generate the answer choices
+  function showAnswers () {
+    answerContainer.style.display = 'inline'
+    currentAnswerChoices.push(`<div class='choicea'> A : ${currentQuestionSet[counter].answerChoices.a}</div>`)
+    currentAnswerChoices.push(`<div class='choiceb'> B : ${currentQuestionSet[counter].answerChoices.b}</div>`)
+    currentAnswerChoices.push(`<div class='choicec'> C : ${currentQuestionSet[counter].answerChoices.c}</div>`)
+    // attempt at a for loop to keep code DRY
+    // for (letter in currentQuestionSet[counter].answerChoices) {
+    //   currentAnswerChoices.push("<div class='choice'" + `${letter}` + ">" + `${letter}` + " : " + `${currentQuestionSet[counter].answerChoices.letter}` + "</div>")
+    // }
+    answerContainer.innerHTML = currentAnswerChoices.join('<br>')
+  }
+
+  // create event listener for each answer choice to change color when the mouse hovers
+  answerContainer.addEventListener('mouseover', function (e) {
+    e.target.style.color = '#a2c1a4'
+  })
+
+  // create event listener for each answer choice to change color back when the hovering mouse leaves
+  answerContainer.addEventListener('mouseout', function (e) {
+    e.target.style.color = '#55595d'
+  })
+
+  // create event listener to submit answer choice, show a message that the message is either right or wrong, and generate a next question button
+  answerContainer.addEventListener('click', function (e) {
+    var wrongDiv = `<div class='wrong'>Not quite!</div>`
+    var correctDiv = `<div class='correct'>Correct! You have been awarded 1 point!</div>`
+    if (e.target.className === `choice${currentQuestionSet[counter].correctAnswer}`) {
+      answerContainer.style.display = 'none'
+      questionContainer.innerHTML = correctDiv
+      nextButton.style.display = 'inline'
+      score += 1
+      scoreContainer.innerHTML = score
+    } else {
+      answerContainer.style.display = 'none'
+      questionContainer.innerHTML = wrongDiv
+      nextButton.style.display = 'inline'
+    }
+  })
+
+  // create an event listener to generate the next question
+  nextButton.addEventListener('click', generateNextQuestion)
+
+  // attempt at event listener to click enter for 'next question' button:
+  // if (questionContainer.innerHTML === wrongDiv || questionContainer.innerHTML === correctDiv) {
+  //   window.addEventListener('keypress', generateNextQuestion)
+  // }
+
+  // increase the counter number to iterate through the next question and answers in the triviaQuestions array
+  function generateNextQuestion () {
+    if (counter < 5) {
+      currentAnswerChoices = []
+      counter += 1
+      showQuestions()
+    } else {
+      counter += 1
+      showFinalScore()
+    }
+  }
+
+  // show final score and corresponding message
+  function showFinalScore () {
+    stopTimer()
+    timer = 0
+    timerText.innerHTML = '00:00'
+    if (score === 0) {
+      nextButton.style.display = 'none'
+      questionContainer.innerHTML = 'Game over! Unfortunately, you did not score. You need to brush up on your gluten knowledge!'
+    } else if (score < (counter - 3)) {
+      nextButton.style.display = 'none'
+      questionContainer.innerHTML = `Your final score is ${score}. Looks like you need to brush up on your gluten knowledge.`
+    } else if (score <= counter) {
+      nextButton.style.display = 'none'
+      questionContainer.innerHTML = `Your final score is ${score}. Looks like you know a thing or two about gluten!`
+    }
+    returnHomeButton.style.display = 'inline'
+  }
+
+  // create event listener for home button to reset to the start page
+  returnHomeButton.addEventListener('click', resetPage)
+
+  // create a function to reset to the start page to begin the quiz again
+  function resetPage () {
+    questionContainer.innerHTML = "So you want to learn more about gluten? You've come to right place. Welcome to <a class='game-title'>Can I Eat This?</a>, a game on gluten. You'll learn all about this pervasive protein - what it is, what it's in, and how to avoid it. Click the button below to begin.<br>"
+    returnHomeButton.style.display = 'none'
+    startButton.style.display = 'inline'
+    counter = 0
+    score = 0
+    timer = 60
+    scoreContainer.innerHTML = score
+    currentQuestionSet = []
+    currentAnswerChoices = []
+  }
+
+  var gameTimer
+
+  function stopTimer () {
+    console.log(gameTimer)
+    clearInterval(gameTimer)
+  }
+
+  // create timer
+  function startTimer () {
+    gameTimer = setInterval(function () {
+      timer--
+      if (timer === 0) {
+        timerText.innerHTML = '00:00'
+        stopTimer()
+        if (counter <= 9) {
+          questionContainer.innerHTML = 'Game Over! It appears you are not a master of gluten knowledge. Try again!'
+          returnHomeButton.style.display = 'inline'
+          answerContainer.style.display = 'none'
+        }
+      } else if (timer < 10) {
+        timerText.innerHTML = '00:0' + timer
+      } else if (timer >= 10) {
+        timerText.innerHTML = '00:' + timer
+      }
+    }, 1000)
+  }
 }
+
+runGame()
--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-// array holding the trivia questions and corresponding answer choices
+// array holding the trivia questions and corresponding answer choices for category "What is Celiac Disease?"
 var celiacQuestions = [
   {
     question: '1. What is gluten?',
@@ -53,102 +53,13 @@ var celiacQuestions = [
       c: 'administer a shot'
     },
     correctAnswer: 'b'
-  },
-  {
-    question: '7. Which of the following foods contains gluten?',
-    answerChoices: {
-      a: 'soy sauce',
-      b: 'olive oil',
-      c: 'rice'
-    },
-    correctAnswer: 'a'
-  },
-  {
-    question: '8. Which of the following foods contains gluten?',
-    answerChoices: {
-      a: 'cheese',
-      b: 'balsamic vinegar',
-      c: 'malt vinegar'
-    },
-    correctAnswer: 'c'
-  },
-  {
-    question: '9. Which of the following foods contains gluten?',
-    answerChoices: {
-      a: 'gravy',
-      b: 'coconut oil',
-      c: 'nuts'
-    },
-    correctAnswer: 'a'
-  },
-  {
-    question: '10. Are oats gluten free',
-    answerChoices: {
-      a: 'Yes',
-      b: 'No',
-      c: 'Technically oats are gluten free, but they are often contaminated by mills that also process wheat.'
-    },
-    correctAnswer: 'c'
   }
 ]
 
+// array holding the trivia questions and corresponding answer choices for category "Can I Eat This?"
 var foodQuestions = [
   {
-    question: '1. food',
-    answerChoices: {
-      a: 'a seed found in plants',
-      b: 'a protein comprised of two molecules, glutenin and gliadin',
-      c: 'glue that holds food together'
-    },
-    correctAnswer: 'b'
-  },
-  {
-    question: '2. food',
-    answerChoices: {
-      a: 'corn and wheat',
-      b: 'wheat, barley, and rye',
-      c: 'rice and corn'
-    },
-    correctAnswer: 'b'
-  },
-  {
-    question: '3. food',
-    answerChoices: {
-      a: 'an allergy',
-      b: 'a contagious illness',
-      c: 'a genetic autoimmune disease'
-    },
-    correctAnswer: 'c'
-  },
-  {
-    question: '4. food',
-    answerChoices: {
-      a: 'small intestine',
-      b: 'stomach',
-      c: 'esophagus'
-    },
-    correctAnswer: 'a'
-  },
-  {
-    question: '5. food',
-    answerChoices: {
-      a: 'triggers an immune response, damaging the lining of the intestine over time (and resulting in malabsorption)',
-      b: 'prevents your stomach from being able to digest any foods',
-      c: 'blocks the production of digestive acid in your stomach'
-    },
-    correctAnswer: 'a'
-  },
-  {
-    question: '6. food',
-    answerChoices: {
-      a: 'take a pill',
-      b: 'gluten free diet',
-      c: 'administer a shot'
-    },
-    correctAnswer: 'b'
-  },
-  {
-    question: '7. food',
+    question: '1. Which of the following foods contains gluten?',
     answerChoices: {
       a: 'soy sauce',
       b: 'olive oil',
@@ -157,7 +68,7 @@ var foodQuestions = [
     correctAnswer: 'a'
   },
   {
-    question: '8. food',
+    question: '2. Which of the following foods contains gluten?',
     answerChoices: {
       a: 'cheese',
       b: 'balsamic vinegar',
@@ -166,7 +77,7 @@ var foodQuestions = [
     correctAnswer: 'c'
   },
   {
-    question: '9. food',
+    question: '3. Which of the following foods contains gluten?',
     answerChoices: {
       a: 'gravy',
       b: 'coconut oil',
@@ -175,102 +86,30 @@ var foodQuestions = [
     correctAnswer: 'a'
   },
   {
-    question: '10. food',
+    question: '4. Are oats gluten free?',
     answerChoices: {
       a: 'Yes',
       b: 'No',
       c: 'Technically oats are gluten free, but they are often contaminated by mills that also process wheat.'
     },
     correctAnswer: 'c'
+  },
+  {
+    question: '5. Which of the following foods contains gluten?',
+    answerChoices: {
+      a: 'Fruity Pebbles',
+      b: 'Rice Krispies',
+      c: 'Cheerios'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: '6. Which of the following foods contains gluten?',
+    answerChoices: {
+      a: 'Skittles',
+      b: 'Twizzlers',
+      c: 'Starburst'
+    },
+    correctAnswer: 'b'
   }
 ]
-
-//  answerChoices in array:
-// var triviaQuestions = [
-//   {
-//     question: 'Question 1',
-//     answerChoices: ['A', 'B', 'C'],
-//     correctAnswer: 'b'
-//   },
-//   {
-//     question: 'Question 2',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'b'
-//   },
-//   {
-//     question: 'Question 3',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'c'
-//   },
-//   {
-//     question: 'Question 4',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'a'
-//   },
-//   {
-//     question: 'Question 5',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'a'
-//   },
-//   {
-//     question: 'Question 6',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'b'
-//   },
-//   {
-//     question: 'Question 7',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'a'
-//   },
-//   {
-//     question: 'Question 8',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'c'
-//   },
-//   {
-//     question: 'Question 9',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'a'
-//   },
-//   {
-//     question: 'Question 10',
-//     answerChoices: {
-//       a: 'A',
-//       b: 'B',
-//       c: 'C'
-//     },
-//     correctAnswer: 'b'
-//   }
-// ]
